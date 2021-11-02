62f83154bf4642de0e5e24b7c572116ed68bdd2f
csy <1608088355@qq.com>
2021-11-02 16:09:00 +0800

    fix merge problems

---
+++ src/main/java/cn/malong/blog/dao/BlogsMapper.java
@@ -0,0 +1,20 @@
+package cn.malong.blog.dao;
+
+import cn.malong.blog.pojo.Blog;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Mapper
+@Repository
+public interface BlogsMapper {
+
+    List<Blog> getBlogsByLimit(@Param("startIndex")int startIndex,@Param("pageSize")int pageSize);
+
+    List<Blog> getBlogsByLimitByTitle(@Param("startIndex")int startIndex,@Param("pageSize")int pageSize,@Param("title") String title);
+
+    List<Blog> getBlogsByLimitByUser(@Param("startIndex")int startIndex,@Param("pageSize")int pageSize,@Param("username") String username);
+
+    List<Blog> getBlogsByLimitByTitleAndUser(@Param("startIndex")int startIndex,@Param("pageSize")int pageSize,@Param("title") String title,@Param("username") String username);
+}
