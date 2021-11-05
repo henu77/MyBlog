4a14fcee83a7883d8de3bbea60d4500c15bf2dc9
henu77 <1289596706@qq.com>
2021-11-05 20:56:45 +0800

    前台页面优化

--- src/main/java/cn/malong/blog/controller/BlogsController.java
+++ src/main/java/cn/malong/blog/controller/BlogsController.java
@@ -3,10 +3,7 @@ package cn.malong.blog.controller;
 import cn.malong.blog.pojo.Blog;
 import cn.malong.blog.service.BlogsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Csy
