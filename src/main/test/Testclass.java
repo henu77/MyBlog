6ce22526795fa7802ce6cf301973d85a416ec230
henu77 <1289596706@qq.com>
2021-11-08 08:35:40 +0800

    测试211

--- src/main/test/Testclass.java
+++ src/main/test/Testclass.java
@@ -1,48 +1,40 @@
 public class Testclass {
 
-    public void conflict(){
+    public void conflict() {
         System.out.println("resolve conflict0");
     }
-    public void conflict1(){
+
+    public void conflict1() {
         System.out.println("resolve conflict1");
     }
-    public void conflict2(){
+
+    public void conflict2() {
         System.out.println("resolve conflict2");
     }
-    public void conflict3(){
+
+    public void conflict3() {
         System.out.println("resolve conflict3");
     }
-    public void conflict4(){
+
+    public void conflict4() {
         System.out.println("resolve conflict4");
-    public void conflict() {
-        System.out.println("resolve conflict");
-    }
+        public void conflict () {
+            System.out.println("resolve conflict");
+        }
 
-    private void hello1() {
-        System.out.println("hello");
-    }
+        public void haha () {
 
-    private void hello2() {
-        System.out.println("hello");
-    }
+        }
 
-    private void hello3() {
-        System.out.println("hello");
-    }
+        private void hello5 () {
+            System.out.println("hello");
+        }
 
-    private void hello4() {
-        System.out.println("hello");
-    }
+        private void hello6 () {
+            System.out.println("hello");
+        }
 
-    private void hello5() {
-        System.out.println("hello");
+        private void hello7 () {
+            System.out.println("hello");
+        }
     }
-
-    private void hello6() {
-        System.out.println("hello");
-    }
-
-    private void hello7() {
-        System.out.println("hello");
-    }
-}
