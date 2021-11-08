86c8761cd08ba40a7b295cb8e3a79328f8fcde44
csy <1608088355@qq.com>
2021-11-08 08:29:46 +0800

    #

--- src/main/test/Testclass.java
+++ src/main/test/Testclass.java
@@ -1,5 +1,13 @@
 public class Testclass {
+
+    public void conflict(){
+        System.out.println("resolve conflict");
+    }
     public static void main(String[] args) {
         System.out.println("hello");
+    }
+
+    public void hello(){
+        System.out.println("hello world");
     }
 }
