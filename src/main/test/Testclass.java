6fb0cb9b7c737500872a5c24ad9b5f65511b291c
csy <1608088355@qq.com>
2021-11-08 08:33:10 +0800

    #

--- src/main/test/Testclass.java
+++ src/main/test/Testclass.java
@@ -1,6 +1,18 @@
 public class Testclass {
 
     public void conflict(){
-        System.out.println("resolve conflict");
+        System.out.println("resolve conflict0");
+    }
+    public void conflict1(){
+        System.out.println("resolve conflict1");
+    }
+    public void conflict2(){
+        System.out.println("resolve conflict2");
+    }
+    public void conflict3(){
+        System.out.println("resolve conflict3");
+    }
+    public void conflict4(){
+        System.out.println("resolve conflict4");
     }
 }
