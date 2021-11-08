d2ec361db8dbdac1ad3a2d379ddaaff83912afd1
henu77 <1289596706@qq.com>
2021-11-08 08:32:35 +0800

    测试211

--- src/main/test/Testclass.java
+++ src/main/test/Testclass.java
@@ -1,6 +1,34 @@
 public class Testclass {
 
-    public void conflict(){
+    public void conflict() {
         System.out.println("resolve conflict");
+    }
+
+    private void hello1() {
+        System.out.println("hello");
+    }
+
+    private void hello2() {
+        System.out.println("hello");
+    }
+
+    private void hello3() {
+        System.out.println("hello");
+    }
+
+    private void hello4() {
+        System.out.println("hello");
+    }
+
+    private void hello5() {
+        System.out.println("hello");
+    }
+
+    private void hello6() {
+        System.out.println("hello");
+    }
+
+    private void hello7() {
+        System.out.println("hello");
     }
 }
